5c19ccf83c77d9e7d41e7f57ca2b2e51b99169c2
Bosco Mutunga <boscomutunga@gmail.com>
2014-12-17 22:42:18 +0300

    initial cluster support in main app file

--- meadowlark.js
+++ meadowlark.js
@@ -139,12 +139,21 @@ app.use(function(req, res){
     res.render('500');
 });
 
-app.listen(app.get('port'), function(){
-    console.log('Express started in '+ app.get('env') +
-		' mode on http://localhost:' + app.get('port') +
-		'; press Ctrl-C to terminate.');
-});
+function startServer(){
+    app.listen(app.get('port'), function(){
+	console.log('Express started in '+ app.get('env') +
+		    ' mode on http://localhost:' + app.get('port') +
+		    '; press Ctrl-C to terminate.');
+    });
+}
 
+if(require.main == module){
+    // application run directly; start app server
+    startServer();
+}else{
+    // application imported as a module via "require": export function
+    module.exports = startServer;
+}
 function getWeatherData(){
     return {
 	locations: [
