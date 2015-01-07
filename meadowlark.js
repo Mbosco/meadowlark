3b50f1612f6ac8cb5343ee73a52ea680b4320f6c
Bosco Mutunga <boscomutunga@gmail.com>
2015-01-07 07:34:03 +0300

    advanced error handling

--- meadowlark.js
+++ meadowlark.js
@@ -44,6 +44,48 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 // middleware
+app.use(function(req, res, next){
+    //create a domain for this request
+    var domain = require('domain').create();
+    //handle errors on this domain
+    domain.on('error', function(err){
+	console.error('DOMAIN ERROR CAUGHT\N', err.stack);
+	try{
+	    //failsafe shutdown in 5 seconds
+	    setTimeout(function(){
+		console.error('Failsafe shutdown.');
+		process.exit(1);
+	    }, 5000);
+	    
+	    // disconnect from the cluster
+	    var worker = require('cluster').worker;
+	    if(worker) worker.disconnect();
+	    
+	    // stop taking new requests
+	    server.close();
+
+	    try{
+		//attempt to use express error route
+		next(err);
+	    }catch(err){
+		// if Express error route failed, try
+		// plain Node response
+		console.error('Express error mechanism failed.\n', err.stack);
+		res.statusCode = 500;
+		res.setHeader('content-type', 'text/plain');
+		res.end('Server error.');
+	    }
+	}catch(err){
+	    console.error('Unable to send 500 response.\n', err.stack);
+	}
+    });
+    // add the request and response objects to the domain
+    domain.add(req);
+    domain.add(res);
+
+    // execute the rest of the request chain in the domain
+    domain.run(next);
+});
 app.use(express.static(__dirname + '/public'));
 app.use(require('body-parser')());
 app.use(require('cookie-parser')(credentials.cookieSecret));
@@ -101,6 +143,15 @@ app.get('/contest/vacation-photo/', function(req, res){
     });
 });
 
+app.get('/fail', function(req, res){
+    throw new Error('Nope!');
+});
+app.get('/epic-fail', function(req, res){
+    process.nextTick(function(){
+	throw new Error('Kaboom!');
+    });
+});
+
 app.post('/contest/vacation-photo/:year/:month', function(req, res){
     var form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files){
@@ -139,17 +190,24 @@ app.use(function(req, res){
     res.render('500');
 });
 
+// error handler
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500).render('500');
+});
+
 function startServer(){
-    app.listen(app.get('port'), function(){
+    var server = app.listen(app.get('port'), function(){
 	console.log('Express started in '+ app.get('env') +
 		    ' mode on http://localhost:' + app.get('port') +
 		    '; press Ctrl-C to terminate.');
     });
+    return server;
 }
 
 if(require.main == module){
     // application run directly; start app server
-    startServer();
+    var server = startServer();
 }else{
     // application imported as a module via "require": export function
     module.exports = startServer;
